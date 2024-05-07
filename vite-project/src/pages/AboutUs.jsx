680240056a784bdcacb72df6b8ab697c913b7f9b
israel <lisrael1989@gmail.com>
2024-05-07 18:54:28 +0300

    adding about me section -v1

--- vite-project/src/pages/AboutUs.jsx
+++ vite-project/src/pages/AboutUs.jsx
@@ -1,42 +1,54 @@
-// import { GoogleMap } from "../cmps/GoogleMap";
 
-// export function AboutUs() {
-//   return (
-//     <section className="about-page">
-
-//       <div className="contact-us-section">
-//         <h3>Contact us:</h3>
-//         <p className="email-about">📩 <span>Litvak-toy-shop@gmail.com</span></p>
-//         <p className="address-about">🏬 Bilo 40, Ness-ziona</p>
-//         <p className="phone-about">☎️ <span>+972-528-654-789</span></p>
-//         {<GoogleMap />}
-//       </div>
-//     </section>
-
-//   )
-// }
-
-// About.js
-
-import React from 'react';
+import { useEffect } from "react";
+import Typed from 'typed.js';
 
 export function AboutUs() {
+
+  useEffect(() => {
+    const typed = new Typed('.typing-2', {
+      strings: [
+        'Fullstack Developer',
+        'Passionate Coder',
+        '♥ {CSS}',
+        'Problem Solver',
+        'JavaScript Ninja'
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="App aboutUs" id="AboutUs">
-      <section className="App-main-section">
-        <img src="https://sanaullah.netlify.app/navlogov2.svg" className="App-logo" alt="logo" />
-        <p>
-          about me section
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test test test
-        </a>
+      <section className="App-main-section AboutUs  ">
+        <div className="max-width">
+          <h2 className="title-about-me">About Me</h2>
+          <h5 className='sub-title-about-me'>— Who I Am ? —</h5>
+
+          <div className="about-content ">
+            <div className="left ">
+              <img className="israel-img" src="https://res.cloudinary.com/dpixrsdwh/image/upload/v1715088963/israel_e8xrgk.jpg" alt="Profile Image 626x626" />
+            </div>
+            <div className="right">
+              <div className="text">I'm israel and I'm a <span className="typing-2"></span></div>
+              <p>Seeking an entry-level opportunity with an esteemed organization.
+                <br /> where I can utilize my skills & enhance learning in the field of work.
+                Capable of mastering new technologies.</p>
+
+              <div className="text">Why Work With Me</div>
+              <p>I'm a great communicator & love to invest the necessary time to understand the customer's problem very well.</p>
+
+              <a href="https://drive.google.com/file/d/1hfs1s9xjIIILrTzCACn8Ro7ajdjsm8su/view?usp=sharing" target="blank">Download CV</a>
+            </div>
+          </div>
+        </div>
       </section>
+
     </div>
   );
 }
